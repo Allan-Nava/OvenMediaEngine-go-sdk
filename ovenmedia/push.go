7d639de416ff3c72b802e567026aa2836219ff83
Allan Nava <allannava95@gmail.com>
2023-01-25 10:58:25 +0100

    updated start

--- ovenmedia/push.go
+++ ovenmedia/push.go
@@ -1,19 +1,16 @@
 package ovenmedia
 
+import (
+	"fmt"
+)
 
 // POST http://1.2.3.4:8081/v1/vhosts/default/apps/app:startPush
 func (o *OvenMedia) StartPush(appName string)  error{
-    resp, err := o.RestClient.R().
-		SetHeader("Accept", "application/json").
-		Get(o.Url)
-	//
-	if err != nil {
-		return err
-	}
-	//
-	if !strings.Contains(resp.Status(), "200") {
-		o.DebugPrint(fmt.Sprintf("resp -> %v", resp))
-		return errors.New("could not connect OvenMediaEngine")
-	}
+    //
+	resp, err := RestyPost(fmt.Sprintf("/v1/vhosts/default/apps/%s:startPush", appName), body)
+    if err != nil{
+        return err
+    }
+    fmt.Println("resp")
 	return nil
 }
--- ovenmedia/virtualhost.go
+++ ovenmedia/virtualhost.go
@@ -16,19 +16,10 @@ func (o *OvenMedia) CreateVirtualHost(name string) error {
 			},
 		},
 	}
-	resp, err := o.RestClient.R().
-		SetHeader("Accept", "application/json").
-		SetBody(body).
-		SetResult(ResponseVirtualHost{}).
-		Post(o.Url)
-	//
-	if err != nil {
-		return err
-	}
-	if !strings.Contains(resp.Status(), "200") {
-		o.DebugPrint(fmt.Sprintf("resp -> %v", resp))
-		return errors.New(resp.Status())
-	}
+	resp, err := RestyPost("/v1/hosts", body)
+    if err != nil{
+        return err
+    }
 	//
 	virtualHost := resp.Result().(*ResponseVirtualHost)
 	if virtualHost == nil {
@@ -40,18 +31,10 @@ func (o *OvenMedia) CreateVirtualHost(name string) error {
 // GET http://1.2.3.4:8081/v1/vhosts
 func (o *OvenMedia) GetAllVirtualHosts() (*ResponseVirtualList, error) {
 	//
-	resp, err := o.RestClient.R().
-		SetHeader("Accept", "application/json").
-		SetResult(ResponseVirtualList{}).
-		Post(o.Url)
-	//
-	if err != nil {
-		return nil, err
-	}
-	if !strings.Contains(resp.Status(), "200") {
-		o.DebugPrint(fmt.Sprintf("resp -> %v", resp))
-		return nil, errors.New(resp.Status())
-	}
+	resp, err := RestyGet("/v1/hosts")
+    if err != nil{
+        return err
+    }
 	//
 	virtualList := resp.Result().(*ResponseVirtualList)
 	if virtualList == nil {
