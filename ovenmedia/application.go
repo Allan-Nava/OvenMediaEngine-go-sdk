3a6b6dff7c9e854859c5f3eeef47c8209e948d5e
Paxx <daniel.botta@hiway.media>
2023-04-12 11:35:07 +0200

    feat: added the response pushes and the app pushes endpoint

--- ovenmedia/application.go
+++ ovenmedia/application.go
@@ -20,17 +20,3 @@ func (o *ovenMedia) GetApplications(host string) (*ResponseVirtualList, error) {
 	}
 	return &obj, nil
 }
-
-func (o *ovenMedia) GetApplicationPushes(host string, application string) (*ResponsePushes, error) {
-	url := fmt.Sprintf("%s/%s/apps/%s", V1_HOSTS, host, application)
-	resp, err := o.restyPost(url, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	var obj ResponsePushes
-	if err := json.Unmarshal(resp.Body(), &obj); err != nil {
-		return nil, err
-	}
-	return &obj, nil
-}
