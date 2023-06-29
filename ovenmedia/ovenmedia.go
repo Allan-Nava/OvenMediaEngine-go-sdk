7d122e3799e6078674e774df76b7b50802e205fa
Paxx <daniel.botta@hiway.media>
2023-06-29 15:31:37 +0200

    feat: stop recording with url

--- ovenmedia/ovenmedia.go
+++ ovenmedia/ovenmedia.go
@@ -27,7 +27,7 @@ type IOvenMediaClient interface {
 	GetAllPushes(vHost string, appName string) (*ResponsePushes, error)
 	// Recording
 	StartRecording(url string, vHost string, appName string, body RequestRecordingStart) (*ResponseRecordingStart, error)
-	StopRecording(vHost string, appName string, body RequestRecordingStop) (*ResponseRecordingStart, error)
+	StopRecording(url string, vHost string, appName string, body RequestRecordingStop) (*ResponseRecordingStart, error)
 	GetRecordingState(vHost string, appName string, body *RequestRecordingStop) (*ResponseRecordingStart, error)
 	// Stats
 	GetStatsVhosts(vHost string) (*ResponseStats, error)
